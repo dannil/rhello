116d2717a41bdb6d1eb5603c59d712912017bd4a
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2022-03-13 22:06:59 +0100

    Add Lucas numbers

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ fn main() {
     let some_value = 123;
     println!("Hello, world! {}", some_value);
 
-    println!("Please insert the nth position of the Fib number: ");
-    let nth_pos: u32 = read!();
+    println!("Please insert the nth position of the Fibonacci/Lucas number: ");
+    let mut nth_pos: u32 = read!();
+    nth_pos -= 1;
 
-    println!("Result is: {}", math::fibonacci(nth_pos));
+    println!(
+        "Result is: [Fib={}, Lucas={}]",
+        math::fibonacci(nth_pos),
+        math::lucas(nth_pos)
+    );
 }
--- src/math.rs
+++ src/math.rs
@@ -1,11 +1,19 @@
-pub fn fibonacci(pos: u32) -> u32 {
-    let mut vector = vec![0, 1];
+fn fib_sequence(first: u32, second: u32, pos: u32) -> u32 {
+    let mut vector = vec![first, second];
     for _ in 0..pos {
         let temp: u32 = vector[1];
         vector[1] = vector[0] + vector[1];
         vector[0] = temp;
     }
     return vector[0];
+}
+
+pub fn lucas(pos: u32) -> u32 {
+    return fib_sequence(2, 1, pos);
+}
+
+pub fn fibonacci(pos: u32) -> u32 {
+    return fib_sequence(0, 1, pos);
 }
 
 #[cfg(test)]
@@ -18,6 +26,14 @@ mod tests {
         let vector = vec![0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
         for (i, &elem) in vector.iter().enumerate() {
             assert_eq!(fibonacci(i as u32), elem);
+        }
+    }
+
+    #[test]
+    fn test_lucas_10_first() {
+        let vector = vec![2, 1, 3, 4, 7, 11, 18, 29, 47, 76];
+        for (i, &elem) in vector.iter().enumerate() {
+            assert_eq!(lucas(i as u32), elem);
         }
     }
 }
